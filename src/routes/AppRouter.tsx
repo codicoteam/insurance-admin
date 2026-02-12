da927c7a1cdbe63a065bfefdb5aadc2fce7c31ea
JoelK <munhenzvajoelk@gmail.com>
2026-02-13 00:59:25 +0200

    Reporting and Analytics App Router Update

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -15,8 +15,15 @@ import VerificationQueue from "../pages/verification";
 import ManualReviewScreen from "../pages/manualreview";
 import Rules from "../pages/rules";
 import KYCReportsScreen from "../pages/reports";
+import PortfolioKPIsScreen from "../pages/reporting-kpis";
+import ClaimsAnalyticsScreen from "../pages/reporting-claims";
+import SalesFunnelScreen from "../pages/reporting-sales";
+import CustomerAnalyticsScreen from "../pages/reporting-customers";
+import OperationalReportsScreen from "../pages/reporting-operational";
+import RegulatoryReportsScreen from "../pages/reporting-regulatory";
+import DataExportsScreen from "../pages/reporting-exports";
 
- export default function AppRouter () {
+export default function AppRouter () {
 
   return (
     <BrowserRouter>
@@ -33,14 +40,17 @@ import KYCReportsScreen from "../pages/reports";
         <Route path="/users/reviews" element={<AccessReviewScreen/>} />
         <Route path="/users/audit" element={<AuditTrailScreen/>} />
         <Route path="/kyc/queue" element={<VerificationQueue/>} />
-         <Route path="/kyc/review" element={<ManualReviewScreen/>} />
-         <Route path="/kyc/rules" element={<Rules/>} />
-         <Route path="/kyc/reports" element={<KYCReportsScreen/>} />
-        
-        
-        
+        <Route path="/kyc/review" element={<ManualReviewScreen/>} />
+        <Route path="/kyc/rules" element={<Rules/>} />
+        <Route path="/kyc/reports" element={<KYCReportsScreen/>} />
+        <Route path="/reporting/kpis" element={<PortfolioKPIsScreen/>} />
+        <Route path="/reporting/claims" element={<ClaimsAnalyticsScreen/>} />
+        <Route path="/reporting/sales" element={<SalesFunnelScreen/>} />
+        <Route path="/reporting/customers" element={<CustomerAnalyticsScreen/>} />
+        <Route path="/reporting/operational" element={<OperationalReportsScreen/>} />
+        <Route path="/reporting/regulatory" element={<RegulatoryReportsScreen/>} />
+        <Route path="/reporting/exports" element={<DataExportsScreen/>} />
       </Routes>
     </BrowserRouter>
   );
 }
-//
